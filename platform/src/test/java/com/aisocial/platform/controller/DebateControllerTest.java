1bd6a711cf3307ec687f4e040243b0a86edbb0e9
mmcgough8 <mwm9490@icloud.com>
2026-01-14 11:57:01 -0500

    "Removed not used import"

--- platform/src/test/java/com/aisocial/platform/controller/DebateControllerTest.java
+++ platform/src/test/java/com/aisocial/platform/controller/DebateControllerTest.java
@@ -27,7 +27,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import static org.mockito.ArgumentMatchers.any;
+
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -95,7 +95,7 @@ class DebateControllerTest {
     void shouldCreateChallenge() throws Exception {
         CreateDebateRequestDTO request = new CreateDebateRequestDTO(defenderId, "Test topic");
 
-        when(debateService.createChallenge(eq(challengerId), eq(defenderId), eq("Test topic")))
+        when(debateService.createChallenge((challengerId), eq(defenderId), eq("Test topic")))
                 .thenReturn(debateDTO);
 
         mockMvc.perform(post("/api/debates")
