d1408c5d2b35b5ce5d0b8b5b823803c4ebf36e48
FrankMontgomery063 <montgomeryfrank.fm@gmail.com>
2026-01-15 15:55:43 -0500

    "created followservice.js"

---
+++ frontend/src/services/followService.js
@@ -0,0 +1,35 @@
+
+import api from './api';
+
+const followService = {
+  followUser: async (currentUserId, targetUserId) => {
+    await api.post(`/api/users/${targetUserId}/follow`, null, {
+      headers: { 'X-User-Id': currentUserId }
+    });
+  },
+
+  unfollowUser: async (currentUserId, targetUserId) => {
+    await api.delete(`/api/users/${targetUserId}/follow`, {
+      headers: { 'X-User-Id': currentUserId }
+    });
+  },
+
+  getFollowers: async (userId) => {
+    const response = await api.get(`/api/users/${userId}/followers`);
+    return response.data;
+  },
+
+  getFollowing: async (userId) => {
+    const response = await api.get(`/api/users/${userId}/following`);
+    return response.data;
+  },
+
+  isFollowing: async (currentUserId, targetUserId) => {
+    const response = await api.get(`/api/users/${targetUserId}/is-following`, {
+      headers: { 'X-User-Id': currentUserId }
+    });
+    return response.data;
+  },
+};
+
+export default followService;
